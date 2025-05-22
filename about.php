52b90e80d7887ff9c543b4e2a85d8e7ffb02ad48
j3r1ch0123 <110147529+j3r1ch0123@users.noreply.github.com>
2025-05-22 14:36:48 -0400

    Update about.php

--- about.php
+++ about.php
@@ -14,6 +14,7 @@ echo '<p>In order to adopt from DiabolicalBoaz, you must be 18 years or older.</
 echo '<p>To adopt, you must provide us with a picture of your enclosure.</p>';
 echo '<p>Alternatively, you could purchase an enclosure from us.</p>';
 echo '<p>The picture of the enclosure must have you in it, and the enclosure must be in good condition.</p>';
+echo '<p>We do not sell to anyone who uses a snake rack. We find those to be inhumane.</p>';
 echo '<p>Prices depend on the chosen reptile.</p>';
 echo '<p>You can also view the reptiles available for adoption on our <a href="reptiles.php">Reptiles</a> page.</p>';
 echo '<p>If you have any more questions, please contact us through the <a href="contact.php">Contact</a> page.</p>';
